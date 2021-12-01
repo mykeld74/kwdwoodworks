a11d755c3c6811d5b56d5f41741a6811dd6cfe36
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2021-12-01 13:40:50 -0700

    init commit

---
+++ svelte.config.js
@@ -0,0 +1,32 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+import path from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
+		target: '#svelte',
+		vite: {
+			resolve: {
+				alias: {
+					$css: path.resolve('./src/css'),
+					$components: path.resolve('./src/components'),
+					$data: path.resolve('./src/data'),
+					$img: path.resolve('./src/images')
+				}
+			}
+		}
+	}
+};
+
+export default config;
