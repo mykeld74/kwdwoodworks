e4c106699fa185fbed37c15b70b4d48d9811ca44
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2021-12-02 07:26:17 -0700

    Add logo and favicon

--- src/components/image.svelte
+++ src/components/image.svelte
@@ -2,7 +2,7 @@
 	export let source: string;
 	export let altText: string;
 
-	let src = `https://res.cloudinary.com/mykeld74/image/upload/f_auto,q_auto/WestwoodsCC/${source}`;
+	let src = `https://res.cloudinary.com/kwdwoodworks-com/image/upload/f_auto,q_auto/${source}`;
 </script>
 
 <img {src} alt={altText} class={source} loading="lazy" />
