1b15457a6242d4dc4c7d040421f35f74115d6bdf
Mike Daugherty <mikedaugherty@msdwebdesigns.com>
2023-02-20 13:37:08 -0700

    Remove backend files

--- backend/parts/assetSources.js
+++
@@ -1,3 +0,0 @@
-import Cloudinary from 'part:sanity-plugin-asset-source-cloudinary/image-asset-source';
-
-export default [Cloudinary];
---
+++ src/routes/backend/[...catchall]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import config from '$lib/sanity/sanityConfig';
+	import SanityStudio from '$components/sanityStudio.svelte';
+</script>
+
+<SanityStudio {config} />
